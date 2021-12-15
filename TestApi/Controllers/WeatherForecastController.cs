f180afecebd461543d00607bf3e89c29293e532d
zhaonn4 <zhaonn4@AACN-R058>
2021-12-15 13:11:50 +0800

    11

--- TestApi/Controllers/WeatherForecastController.cs
+++ TestApi/Controllers/WeatherForecastController.cs
@@ -14,6 +14,7 @@ namespace TestApi.Controllers
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+      
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
